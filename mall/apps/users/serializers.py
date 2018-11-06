d9396ed1724afc00bbbffe42498bd001b4a0a9d4
ShanXiY <ywg11_123@sina.com>
2018-11-06 16:09:47 +0800

    注册(上)_

--- mall/apps/users/serializers.py
+++ mall/apps/users/serializers.py
@@ -1,5 +1,9 @@
+import re
+
+from django_redis import get_redis_connection
 from rest_framework import serializers
 from users.models import User
+
 
 """
 serializers.ModelSerializer
@@ -21,3 +25,52 @@ class RegisterCreateUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['username','password','mobile']
+
+    """
+    1.字段类型
+    2.字段选项
+    3.单个字段
+    4，多个字段
+
+    1.手机号校验，密码一致，短信校验，是否同意
+
+    手机号就是规则 单个字段
+    是否同意    单个字段
+    密码一致，短信校验       多个字段
+
+    """
+
+    def validate_mobile(self,value):
+        if not re.match('1[3-9]\d{9}',value):
+            raise serializers.ValidationError('手机号格式不满足要求')
+
+
+        #校验之后最终要返回回去
+        return value
+
+    def validate_allow(self,value):
+        if value == 'false':
+            raise serializers.ValidationError('您未同意协议')
+
+        return value
+
+    def validate(self, attrs):
+        #1.密码一致
+        password = attrs.get('password')
+        password2 = attrs.get('password2')
+        mobile = attrs.get('mobile')
+        sms_code = attrs.get('sms_code')
+
+
+        if password != password2:
+            raise serializers.ValidationError('密码不一致')
+
+        #2.短信
+        redis_conn = get_redis_connection('code')
+        sms_code_redis = redis_conn.get('sms_%s'%mobile)
+        if sms_code_redis is None:
+            raise serializers.ValidationError('验证码已过期')
+
+        if sms_code_redis.decode() != sms_code:
+            raise serializers.ValidationError('验证码错误')
+        return attrs
--- mall/apps/users/urls.py
+++ mall/apps/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^usernames/(?P<username>\w{5,20})/count/$',views.RegisterUsernameCountView.as_view(),name='usernamecount'),
+    url(r'^$',views.RegisterCreateUserView.as_view()),
 
 ]
