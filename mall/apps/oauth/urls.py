8979a42bfeb76cd364d00f966b42ea4cb310b55a
ShanXiY <ywg11_123@sina.com>
2018-11-08 19:11:17 +0800

    获取token和openid

--- mall/apps/oauth/urls.py
+++ mall/apps/oauth/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^qq/statues/$',views.OauthQQURLView.as_view()),
+    url(r'^qq/users/$',views.OauthQQUserView.as_view()),
 ]
--- mall/apps/oauth/views.py
+++ mall/apps/oauth/views.py
@@ -14,7 +14,7 @@ GET   /oauth/qq/statues/
 from rest_framework.views import APIView
 from QQLoginTool.QQtool import OAuthQQ
 from django.conf import settings
-# from mall import settings
+from mall import settings
 
 class OauthQQURLView(APIView):
 
@@ -33,5 +33,37 @@ class OauthQQURLView(APIView):
         login_url = oauth.get_qq_url()
 
         return Response({"login_url":login_url})
+
+
+"""
+前段应该 在用户扫描完成之后,跳转到 http://www.meiduo.site:8080/oauth_callback.html?code=6E2E3F64C34ECFE29222EBC390D29196&state=test
+把code 传递给后端,
+
+GET     /oauth/qq/users/?code=xxx
+
+# 1.我们获取到这个code, 通过接口来换去 token
+# 2.有了token,就可以换取 oepnid
+
+
+"""
+from rest_framework import status
+
+class OauthQQUserView(APIView):
+    def get(self,request):
+        code = request.query_params.get('code')
+
+        if code is None:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        #1.我们获取到这个code，通过接口来换取token
+        oauth = OAuthQQ(client_id=settings.QQ_CLIENT_ID,
+                        client_secret=settings.QQ_CLIENT_SECRET,
+                        redirect_uri=settings.QQ_REDIRECT_URI)
+
+        access_token = oauth.get_access_token(code)
+
+        #2.有token就可以换取openid
+        openid = oauth.get_open_id(access_token)
+
+        pass
 
 
