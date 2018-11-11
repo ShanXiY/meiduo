18e02119f33fbfae10ece659efbd2874bcba1dfc
ShanXiY <ywg11_123@sina.com>
2018-11-11 15:02:46 +0800

    省市区数据获取及修改

--- mall/apps/areas/models.py
+++ mall/apps/areas/models.py
@@ -5,6 +5,7 @@ class Area(models.Model):
     行政区划
     """
     name = models.CharField(max_length=20, verbose_name='名称')
+    #related_name='' 可以修改关联模型属性的默认名字，默认名字是 关联模型类名小写_set
     parent = models.ForeignKey('self', on_delete=models.SET_NULL, related_name='subs', null=True, blank=True, verbose_name='上级行政区划')
 
     class Meta:
