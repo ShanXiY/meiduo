0bd1aa9d482c02f7c8b54c6f0873ad967b620cfd
ShanXiY <ywg11_123@sina.com>
2018-11-14 19:28:02 +0800

    获取热销商品的第二种方式

--- mall/apps/goods/views.py
+++ mall/apps/goods/views.py
@@ -17,20 +17,47 @@ from goods.serializers import HotSKUSerializer
 """
 # Create your views here.
 
+"""
+获取商品列表 推荐信息
+
+
+# 1.前段应该传递过来一个分类id,我们应该接收这个分类id
+# 2.根据分类id查询商品数据,并对商品数据进行排序,并获取2个  [SKU,SKU,SKU]
+# 3.我们需要对数据进行  序列化操作(将对象转换为字典)
+# 4.返回数据
+
+GET     goods/categories/cat_id/hotskus/
+
+
+
+"""
+
+
 from rest_framework.views import APIView
 
-class HotSKUView(APIView):
-    def get(self,request,category_id):
+# class HotSKUView(APIView):
+#     def get(self,request,category_id):
+#
+#         # 1.前段应该传递过来一个分类id,我们应该接收这个分类id
+#         # 2.根据分类id查询商品数据,并对商品数据进行排序,并获取2个  [SKU,SKU,SKU]
+#         sku = SKU.objects.filter(category_id=category_id,is_launched=True).order_by('-sales')[:2]
+#
+#
+#         # 3.我们需要对数据进行  序列化操作(将对象转换为字典)
+#         serializer = HotSKUSerializer(sku,many=True)
+#
+#
+#         # 4.返回数据
+#         return Response(serializer.data)
 
-        # 1.前段应该传递过来一个分类id,我们应该接收这个分类id
-        # 2.根据分类id查询商品数据,并对商品数据进行排序,并获取2个  [SKU,SKU,SKU]
-        sku = SKU.objects.filter(category_id=category_id,is_launched=True).order_by('-sales')[:2]
+from rest_framework.generics import ListAPIView
 
+class HotSKUView(ListAPIView):
 
-        # 3.我们需要对数据进行  序列化操作(将对象转换为字典)
-        serializer = HotSKUSerializer(sku,many=True)
+    serializer_class = HotSKUSerializer
 
+    def get_queryset(self):
+        category_id = self.kwargs['category_id']
 
-        # 4.返回数据
-        return Response(serializer.data)
+        return SKU.objects.filter(category_id=category_id,is_launched=True).order_by('-sales')[:2]
 
